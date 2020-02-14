4df54b68f3338553235047e94be66685ae7a25cc
isanch14 <isanch14@stu.southtexascollege.edu>
2020-02-14 13:48:59 -0600

    Added Linting

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
+  <div class='home'>
     <h1>Hello There!</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
-};
+  name: 'Home'
+}
 </script>
